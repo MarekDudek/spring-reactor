ec5c137b29600e8ba2aca0745cdf8d98cb2b50ee
Marek Dudek <marek.dudek@gmail.com>
2021-12-17 13:21:05 +0100

    zookeeper yaml generated

--- src/test/java/md/springreactor/kafka/DockerComposeConfigGenerationTest.java
+++ src/test/java/md/springreactor/kafka/DockerComposeConfigGenerationTest.java
@@ -1,17 +1,22 @@
 package md.springreactor.kafka;
 
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toMap;
 import static java.util.stream.IntStream.rangeClosed;
 import static org.yaml.snakeyaml.DumperOptions.FlowStyle.BLOCK;
@@ -29,12 +34,16 @@ public class DockerComposeConfigGenerationTest
     }
 
     private static final Yaml YAML = new Yaml(OPTIONS);
-    private static final Gson GSON = new Gson();
+    private static final Gson GSON = new GsonBuilder().
+            registerTypeAdapter(new TypeToken<Map<String, Object>>()
+            {
+            }.getType(), new DoubleToIntDeserializer()).
+            create();
 
     @Test
     void original_to_equivalent() throws IOException
     {
-        InputStream stream = getClass().getClassLoader().getResourceAsStream("docker/confluent-platform-orig.yml");
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("docker/confluent-platform-orig.yaml");
         Map<String, Object> compose = YAML.load(stream);
         Writer output = new PrintWriter("./src/test/resources/docker/confluent-platform-equiv.yaml");
         YAML.dump(compose, output);
@@ -45,11 +54,12 @@ public class DockerComposeConfigGenerationTest
     {
         int count = 5;
 
-        InputStream stream = getClass().getClassLoader().getResourceAsStream("docker/confluent-platform-orig.yml");
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("docker/confluent-platform-equiv.yaml");
         Map<String, Object> compose = YAML.load(stream);
 
-        Map<String, Object> deepCopy = GSON.fromJson(GSON.toJson(compose), Map.class);
-        Map<String, Object> services = (Map<String, Object>) deepCopy.get("services");
+        Map<String, Object> services = (Map<String, Object>) compose.get("services");
+
+        String bootstrapServers = rangeClosed(1, count).boxed().map(i -> "broker-" + i + ":9092").collect(joining(","));
 
         Map<String, Object> zookeeper = (Map<String, Object>) services.get("zookeeper");
         services.remove("zookeeper");
@@ -60,12 +70,37 @@ public class DockerComposeConfigGenerationTest
 
         Map<String, Map<String, Object>> brokers =
                 rangeClosed(1, count).boxed().map(number -> {
-                            Map<String, Object> broker = GSON.fromJson(GSON.toJson(oldBroker), Map.class);
+
+                            Map<String, Object> broker = new LinkedHashMap<>();
+
+                            broker.put("image", oldBroker.get("image"));
+                            broker.put("hostname", oldBroker.get("hostname"));
+                            broker.put("container_name", oldBroker.get("container_name"));
+
+                            List<String> oldDependsOn = (List<String>) oldBroker.get("depends_on");
+                            List<Object> dependsOn = new ArrayList<>();
+                            oldDependsOn.forEach(dependsOn::add);
+                            broker.put("depends_on", dependsOn);
+
+                            List<String> oldPorts = (List<String>) oldBroker.get("ports");
+                            List<Object> ports = new ArrayList<>();
+                            oldPorts.forEach(ports::add);
+                            broker.put("ports", ports);
+
+                            Map<String, Object> oldEnvironment = (Map<String, Object>) oldBroker.get("environment");
+                            Map<String, Object> environment = new LinkedHashMap<>();
+                            for (Entry<String, Object> entry : oldEnvironment.entrySet())
+                                environment.put(entry.getKey(), entry.getValue());
+                            broker.put("environment", environment);
+
+
                             String name = "broker-" + number;
                             broker.replace("hostname", name);
                             broker.replace("container_name", name);
-                            Map<String, Object> environment = (Map<String, Object>) broker.get("environment");
+                            environment = (Map<String, Object>) broker.get("environment");
                             environment.replace("KAFKA_BROKER_ID", number);
+                            environment.replace("CONFLUENT_METRICS_REPORTER_BOOTSTRAP_SERVERS", bootstrapServers);
+
                             return broker;
                         }
                 ).collect(toMap(
@@ -81,6 +116,8 @@ public class DockerComposeConfigGenerationTest
         Map<String, Object> schemaRegistry = (Map<String, Object>) services.get("schema-registry");
         services.remove("schema-registry");
         services.put("schema-registry", schemaRegistry);
+        Map<String, Object> environment = (Map<String, Object>) schemaRegistry.get("environment");
+        environment.replace("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", bootstrapServers);
         List<String> dependsOn = (List<String>) schemaRegistry.get("depends_on");
         dependsOn.remove("broker");
         dependsOn.addAll(brokers.keySet());
@@ -88,6 +125,8 @@ public class DockerComposeConfigGenerationTest
         Map<String, Object> connect = (Map<String, Object>) services.get("connect");
         services.remove("connect");
         services.put("connect", connect);
+        environment = (Map<String, Object>) connect.get("environment");
+        environment.replace("CONNECT_BOOTSTRAP_SERVERS", bootstrapServers);
         dependsOn = (List<String>) connect.get("depends_on");
         dependsOn.remove("broker");
         dependsOn.addAll(brokers.keySet());
@@ -95,6 +134,8 @@ public class DockerComposeConfigGenerationTest
         Map<String, Object> controlCenter = (Map<String, Object>) services.get("control-center");
         services.remove("control-center");
         services.put("control-center", controlCenter);
+        environment = (Map<String, Object>) controlCenter.get("environment");
+        environment.replace("CONTROL_CENTER_BOOTSTRAP_SERVERS", bootstrapServers);
         dependsOn = (List<String>) controlCenter.get("depends_on");
         dependsOn.remove("broker");
         dependsOn.addAll(brokers.keySet());
@@ -102,6 +143,8 @@ public class DockerComposeConfigGenerationTest
         Map<String, Object> ksqldbServer = (Map<String, Object>) services.get("ksqldb-server");
         services.remove("ksqldb-server");
         services.put("ksqldb-server", ksqldbServer);
+        environment = (Map<String, Object>) ksqldbServer.get("environment");
+        environment.replace("KSQL_BOOTSTRAP_SERVERS", bootstrapServers);
         dependsOn = (List<String>) ksqldbServer.get("depends_on");
         dependsOn.remove("broker");
         dependsOn.addAll(brokers.keySet());
@@ -116,6 +159,8 @@ public class DockerComposeConfigGenerationTest
         Map<String, Object> ksqlDatagen = (Map<String, Object>) services.get("ksql-datagen");
         services.remove("ksql-datagen");
         services.put("ksql-datagen", ksqlDatagen);
+        environment = (Map<String, Object>) ksqlDatagen.get("environment");
+        environment.replace("STREAMS_BOOTSTRAP_SERVERS", bootstrapServers);
         dependsOn = (List<String>) ksqlDatagen.get("depends_on");
         dependsOn.remove("broker");
         dependsOn.addAll(brokers.keySet());
@@ -123,12 +168,14 @@ public class DockerComposeConfigGenerationTest
         Map<String, Object> restProxy = (Map<String, Object>) services.get("rest-proxy");
         services.remove("rest-proxy");
         services.put("rest-proxy", restProxy);
+        environment = (Map<String, Object>) restProxy.get("environment");
+        environment.replace("KAFKA_REST_BOOTSTRAP_SERVERS", bootstrapServers);
         dependsOn = (List<String>) restProxy.get("depends_on");
         dependsOn.remove("broker");
         dependsOn.addAll(brokers.keySet());
 
         Writer output = new PrintWriter("./src/test/resources/docker/confluent-platform-multi-broker.yaml");
-        YAML.dump(deepCopy, output);
+        YAML.dump(compose, output);
     }
 }
 
