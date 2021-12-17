c4f1240c0f4351587cadca91cfa43605e6208612
Marek Dudek <marek.dudek@gmail.com>
2021-12-18 00:16:26 +0100

    Spring for Kafka

--- src/main/java/md/springreactor/config/KafkaTopicConfig.java
+++ src/main/java/md/springreactor/config/KafkaTopicConfig.java
@@ -1,15 +1,20 @@
 package md.springreactor.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -17,6 +22,7 @@ import java.util.Map;
 import static org.apache.kafka.clients.producer.ProducerConfig.*;
 
 @Configuration
+@Slf4j
 public class KafkaTopicConfig
 {
     @Autowired
@@ -25,11 +31,11 @@ public class KafkaTopicConfig
     @Bean
     public ProducerFactory<String, String> producerFactory()
     {
-        Map<String, Object> props = new LinkedHashMap<>();
-        props.put(BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-        props.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(props);
+        Map<String, Object> configs = new LinkedHashMap<>();
+        configs.put(BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        configs.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configs);
     }
 
     @Bean
@@ -38,15 +44,34 @@ public class KafkaTopicConfig
         return new KafkaTemplate<>(producerFactory());
     }
 
+    @Value("${md.kafka-topic-config.topic-name}")
+    private String topicName;
+
     @Bean
     public NewTopic newTopicCreatedByString()
     {
-        return new NewTopic("new-topic-created-by-spring", 1, (short) 1);
+        return new NewTopic(topicName, 1, (short) 1);
     }
 
     @Scheduled(fixedRate = 1_000)
     public void sendMessage()
     {
-        kafkaTemplate().send("new-topic-created-by-spring", "my message");
+        KafkaTemplate<String, String> template = kafkaTemplate();
+        ListenableFuture<SendResult<String, String>> future = template.send(topicName, "my message");
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>()
+                           {
+                               @Override
+                               public void onSuccess(SendResult<String, String> result)
+                               {
+                                   log.info("Send result: {}", result);
+                               }
+
+                               @Override
+                               public void onFailure(Throwable exc)
+                               {
+                                   log.error("Send error", exc);
+                               }
+                           }
+        );
     }
 }
